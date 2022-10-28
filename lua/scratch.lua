7a5c3d371705a1162b4bdb9877e32ba27d53be5d
Lintao <Lintao.Amons@outlook.com>
2022-10-29 00:30:55 +0800

    check dir existance

--- lua/scratch.lua
+++ lua/scratch.lua
@@ -2,15 +2,12 @@ local config = require("config")
 local M = {}
 
 M.test = function()
-	vim.fn.mkdir(config.scratch_file_dir, "p")
-	vim.notify(config.scratch_file_dir .. " created")
-
-	-- if not vim.fn.isdirectory(config.scratch_file_dir) then
-	-- 	vim.fn.mkdir(config.scratch_file_dir, "p")
-	-- 	vim.notify(config.scratch_file_dir .. " created")
-	-- else
-	-- 	vim.notify(config.scratch_file_dir .. " already exists")
-	-- end
+	if vim.fn.isdirectory(config.scratch_file_dir) ~= 0 then
+		vim.fn.mkdir(config.scratch_file_dir, "p")
+		vim.notify(config.scratch_file_dir .. " created")
+	else
+		vim.notify(config.scratch_file_dir .. " already exists")
+	end
 end
 
 return M
