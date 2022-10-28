9787b21ad44f628f3375d002078f1675fe30467a
Lintao <Lintao.Amons@outlook.com>
2022-10-29 00:32:44 +0800

    revert condition

--- lua/scratch.lua
+++ lua/scratch.lua
@@ -2,7 +2,7 @@ local config = require("config")
 local M = {}
 
 M.test = function()
-	if vim.fn.isdirectory(config.scratch_file_dir) ~= 0 then
+	if vim.fn.isdirectory(config.scratch_file_dir) == 0 then
 		vim.fn.mkdir(config.scratch_file_dir, "p")
 		vim.notify(config.scratch_file_dir .. " created")
 	else
