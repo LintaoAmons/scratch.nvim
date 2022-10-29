b289f6d619eb9b67f74eaccce6253fa1861e5f84
Lintao <Lintao.Amons@outlook.com>
2022-10-29 10:11:18 +0800

    feat: select filetype ui

--- lua/config.lua
+++ lua/config.lua
@@ -1,5 +1,15 @@
 local M = {
 	scratch_file_dir = vim.env.HOME .. "/scratch.nvim",
+	filetypes = { "json", "xml" },
 }
+
+vim.ui.select(M.filetypes, {
+	prompt = "Select filetype",
+	format_item = function(item)
+		return item
+	end,
+}, function(choice)
+	vim.notify(choice)
+end)
 
 return M
