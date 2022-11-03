83899895a76aa107440677a8442781681a549e94
Lintao <Lintao.Amons@outlook.com>
2022-11-04 00:02:34 +0800

    remove print

--- lua/config.lua
+++ lua/config.lua
@@ -8,10 +8,7 @@ local default_config = {
 M.config = default_config
 
 M.setup = function(user_config)
-	print("Called setup")
-	print(vim.inspect(user_config))
 	M.config = vim.tbl_deep_extend("force", default_config, user_config or {})
-	print(vim.inspect(M.config))
 end
 
 return M
