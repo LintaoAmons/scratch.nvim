4c7ab807f4ceafe65276ee096a23b2bd67eff104
Lintao <Lintao.Amons@outlook.com>
2022-11-04 00:01:08 +0800

    init config and bebug log

--- lua/config.lua
+++ lua/config.lua
@@ -1,14 +1,17 @@
 local M = {}
 
-M.default_config = {
+local default_config = {
 	scratch_file_dir = vim.fn.stdpath("cache") .. "/scratch.nvim",
 	filetypes = { "json", "xml", "go", "lua", "js", "py", "sh" },
 }
 
+M.config = default_config
+
 M.setup = function(user_config)
 	print("Called setup")
 	print(vim.inspect(user_config))
-	M.config = vim.tbl_deep_extend("force", M.default_config, user_config or {})
+	M.config = vim.tbl_deep_extend("force", default_config, user_config or {})
+	print(vim.inspect(M.config))
 end
 
 return M
