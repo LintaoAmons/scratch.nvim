281c31c2768ad7d32c5eacd9ed174dd820b7ba8c
Lintao <Lintao.Amons@outlook.com>
2022-11-03 23:57:59 +0800

    don't know why setup didn't called

--- lua/scratch.lua
+++ lua/scratch.lua
@@ -2,9 +2,8 @@ local api = require("api")
 local config = require("config")
 local commands = require("commands")
 
-commands.init()
-
 config.setup()
+commands.init()
 api.setup = config.setup
 
 return api
