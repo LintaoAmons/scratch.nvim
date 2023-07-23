454fb0ffee5071a819f889cd87f8c6f87e7d165a
Lintao <lintao.amons@outlook.com>
2023-07-23 19:02:47 +0800

    rename to flag

--- lua/config.lua
+++ lua/config.lua
@@ -1,149 +1,157 @@
 local M = {}
 
-local CONFIG_FILE_PATH = vim.fn.stdpath("cache") .. "/scratch2.nvim/" .. "configFilePath"
+-- CONFIG_FILE_PATH act like a flag to check if user already init the plugin or not
+-- inside only contains the info about the path where user put there's config json content
+local CONFIG_FILE_FLAG_PATH = vim.fn.stdpath("cache") .. "/scratch.nvim/" .. "configFilePath"
+local DEFAULT_CONFIG_PATH = vim.fn.stdpath("config") .. "/scratch.nvim"
+local logErr = function(msg)
+	vim.notify(msg, vim.log.levels.ERROR, { title = "easy-commands.nvim" })
+end
 
 local default_config = {
-    scratch_file_dir = vim.fn.stdpath("cache") .. "/scratch.nvim",
-    filetypes = {"xml", "go", "lua", "js", "py", "sh"}, -- you can simply put filetype here
-    filetype_details = { -- you can have more control here
-        json = {}, -- not required to put this to `filetypes` table, even though you still can
-        go = {
-            requireDir = true,
-            filename = "main",
-            content = {"package main", "", "func main() {", "  ", "}"},
-            cursor = {
-                location = {4, 2},
-                insert_mode = true
-            }
-        }
-    }
+	scratch_file_dir = DEFAULT_CONFIG_PATH,
+	filetypes = { "xml", "go", "lua", "js", "py", "sh" }, -- you can simply put filetype here
+	filetype_details = { -- you can have more control here
+		json = {}, -- not required to put this to `filetypes` table, even though you still can
+		go = {
+			requireDir = true,
+			filename = "main",
+			content = { "package main", "", "func main() {", "  ", "}" },
+			cursor = {
+				location = { 4, 2 },
+				insert_mode = true,
+			},
+		},
+	},
 }
 
 local function getConfigFilePath()
-    -- read CONFIG_FILE_PATH content as the filepath and return
-    local file = io.open(CONFIG_FILE_PATH, "r")
-    local filepath = file:read("*all")
-    file:close()
-    -- print("getConfigFilePath: " .. filepath)
+	-- read CONFIG_FILE_PATH content as the filepath and return
+	local file = io.open(CONFIG_FILE_FLAG_PATH, "r")
+	local filepath = file:read("*all")
+	file:close()
+	-- print("getConfigFilePath: " .. filepath)
 
-    return filepath
+	return filepath
 end
 
 local function validate_abspath(path)
-    -- Check if path starts with a forward slash
-    if string.sub(path, 1, 1) ~= "/" then
-        return false
-    end
+	-- Check if path starts with a forward slash
+	if string.sub(path, 1, 1) ~= "/" then
+		return false
+	end
 
-    -- Check if path contains any invalid characters
-    if string.match(path, "[^%w/%.%-%_]+") then
-        return false
-    end
+	-- Check if path contains any invalid characters
+	if string.match(path, "[^%w/%.%-%_]+") then
+		return false
+	end
 
-    -- Check if path ends with a forward slash
-    if string.sub(path, -1) == "/" then
-        return false
-    end
+	-- Check if path ends with a forward slash
+	if string.sub(path, -1) == "/" then
+		return false
+	end
 
-    -- Check if path contains any double forward slashes
-    if string.match(path, "//") then
-        return false
-    end
+	-- Check if path contains any double forward slashes
+	if string.match(path, "//") then
+		return false
+	end
 
-    -- if already exist check if it's a file
-    if vim.fn.filereadable(path) == 1 then
-        if vim.fn.isdirectory(path) == 1 then
-            return false
-        end
-    end
+	-- if already exist check if it's a file
+	if vim.fn.filereadable(path) == 1 then
+		if vim.fn.isdirectory(path) == 1 then
+			return false
+		end
+	end
 
-    -- If all checks pass, return true
-    return true
+	-- If all checks pass, return true
+	return true
 end
 
-local function initProcess()
-    -- if CONFIG_FILE_PATH file exist, call getConfigFilePath
-    local defaultConfigFilePath = vim.fn.stdpath("cache") .. "/scratch.nvim/config.json"
-    local currentConfigFilePath = defaultConfigFilePath
-    if vim.fn.filereadable(CONFIG_FILE_PATH) == 1 then
-        currentConfigFilePath = getConfigFilePath()
-    end
+---Init the plugin
+---@param force boolean
+local function initProcess(force)
+	-- if CONFIG_FILE_PATH file exist, don't need init
+	if vim.fn.filereadable(CONFIG_FILE_FLAG_PATH) == 1 and force ~= true then
+		if validate_abspath(getConfigFilePath()) then
+			return
+		else
+			logErr("Invalid path. Please rm `" .. CONFIG_FILE_FLAG_PATH .. "`and try again")
+		end
+	end
 
-    -- ask user to input the abspath of scratch file dir
-    local configFilePath = vim.fn.input("Where you want to put your configuration file (abspath): ",
-        currentConfigFilePath)
-    if validate_abspath(configFilePath) == false then
-        vim.notify("invalid path. Path must be abspath and must be file type")
-        return
-    end
-    -- write the scratch_file_dir into CONFIG_FILE_PATH file
-    local dir_path = vim.fn.fnamemodify(CONFIG_FILE_PATH, ":h")
-    if vim.fn.isdirectory(dir_path) == 0 then
-        vim.fn.mkdir(dir_path, "p")
-    end
-    local file = io.open(CONFIG_FILE_PATH, "w")
-    file:write(configFilePath)
-    file:close()
-    -- write default_config into user defined config file
-    local configFilePath = getConfigFilePath()
-    -- create file and dir of the path is not exist
-    vim.fn.mkdir(vim.fn.fnamemodify(configFilePath, ":h"), "p")
-    local file = io.open(configFilePath, "w")
-    file:write(vim.fn.json_encode(default_config))
-    file:close()
-    vim.notify("Init done, your config file will be at " .. configFilePath)
+	-- ask user to input the abspath of scratch file dir
+	local configFilePath =
+		vim.fn.input("Where you want to put your configuration file (abspath): ", DEFAULT_CONFIG_PATH)
+	if validate_abspath(configFilePath) == false then
+		vim.notify("invalid path. Path must be abspath and must be file type")
+		return
+	end
+
+	-- write the scratch_file_dir into CONFIG_FILE_PATH file
+	local dir_path = vim.fn.fnamemodify(CONFIG_FILE_FLAG_PATH, ":h")
+	if vim.fn.isdirectory(dir_path) == 0 then
+		vim.fn.mkdir(dir_path, "p")
+	end
+
+	local file = io.open(CONFIG_FILE_FLAG_PATH, "w")
+	file:write(configFilePath)
+	file:close()
+
+	-- write default_config into user defined config file
+	-- create file and dir of the path is not exist
+
+	vim.fn.mkdir(vim.fn.fnamemodify(configFilePath, ":h"), "p")
+	file = io.open(configFilePath, "w")
+	file:write(vim.fn.json_encode(default_config))
+	file:close()
+	vim.notify("Init done, your config file will be at " .. configFilePath)
 end
 
 -- Read json file and parse to dictionary
 local function readConfigFile()
-    local configFilePath = getConfigFilePath()
-    -- print("readConfigFile: " .. configFilePath)
+	local configFilePath = getConfigFilePath()
 
-    -- Write the file if it does not exist
-    if vim.fn.filereadable(configFilePath) == 0 then
-        initProcess()
-    end
+	local file = io.open(configFilePath, "r")
+	local json_data = file:read("*all")
+	file:close()
 
-    local file = io.open(configFilePath, "r")
-    local json_data = file:read("*all")
-    file:close()
-
-    return vim.fn.json_decode(json_data)
+	return vim.fn.json_decode(json_data)
 end
 
 -- Expose getConfig function
 function M.getConfig()
-    return readConfigFile()
+	return readConfigFile()
 end
 
 -- Expose editConfig function
 function M.editConfig()
-    vim.cmd(":e " .. getConfigFilePath())
+	vim.cmd(":e " .. getConfigFilePath())
 end
 
 function M.checkConfig()
-    vim.notify(vim.inspect(readConfigFile()))
+	vim.notify(vim.inspect(readConfigFile()))
 end
 
 function M.initConfigInterceptor(fn)
+	local function withInterceptor()
+		if vim.fn.filereadable(CONFIG_FILE_FLAG_PATH) == 0 then
+			vim.notify("here")
+			vim.notify(CONFIG_FILE_FLAG_PATH)
+			initProcess(false)
+		else
+			if validate_abspath(getConfigFilePath()) then
+				return fn()
+			else
+				initProcess(false)
+			end
+		end
+	end
 
-    function withInterceptor()
-        if vim.fn.filereadable(CONFIG_FILE_PATH) == 0 then
-            initProcess()
-        else
-            if validate_abspath(getConfigFilePath()) then
-                return fn()
-            else
-                initProcess()
-            end
-        end
-    end
-
-    return withInterceptor
+	return withInterceptor
 end
 
 function M.initConfig()
-    initProcess()
+	initProcess(true)
 end
 
 return M
--- lua/utils.lua
+++ lua/utils.lua
@@ -2,29 +2,35 @@ local M = {}
 
 -- Initialize the scratch file directory if it does not exist
 function M.initDir(scratch_file_dir)
-    if vim.fn.isdirectory(scratch_file_dir) == 0 then
-        vim.fn.mkdir(scratch_file_dir, "p")
-    end
+	vim.print(scratch_file_dir)
+	vim.print(vim.fn.isdirectory(scratch_file_dir))
+	if vim.fn.filereadable(scratch_file_dir) == 0 then
+		vim.fn.mkdir(scratch_file_dir, "p")
+	else
+		if vim.fn.isdirectory(scratch_file_dir) ~= 0 then
+			vim.notify("Exiting file with the same name: " .. scratch_file_dir)
+		end
+	end
 end
 
 -- Recursively list all files in the specified directory
 function M.listDirectoryRecursive(directory)
-    local files = {}
-    local dir_list = vim.fn.readdir(directory)
+	local files = {}
+	local dir_list = vim.fn.readdir(directory)
 
-    for _, file in ipairs(dir_list) do
-        local path = directory .. '/' .. file
-        if vim.fn.isdirectory(path) == 1 and file ~= '.' and file ~= '..' then
-            local subfiles = M.listDirectoryRecursive(path)
-            for _, subfile in ipairs(subfiles) do
-                files[#files + 1] = subfile
-            end
-        elseif vim.fn.isdirectory(path) == 0 then
-            files[#files + 1] = path
-        end
-    end
+	for _, file in ipairs(dir_list) do
+		local path = directory .. "/" .. file
+		if vim.fn.isdirectory(path) == 1 and file ~= "." and file ~= ".." then
+			local subfiles = M.listDirectoryRecursive(path)
+			for _, subfile in ipairs(subfiles) do
+				files[#files + 1] = subfile
+			end
+		elseif vim.fn.isdirectory(path) == 0 then
+			files[#files + 1] = path
+		end
+	end
 
-    return files
+	return files
 end
 
 return M
