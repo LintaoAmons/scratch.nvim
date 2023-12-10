d691a0f50da5bc92086db04231558e9ee48895be
Oliver <1571880470@qq.com>
2023-12-11 00:01:19 +0800

    feat: delete files step by step

--- lua/scratch/scratch_file.lua
+++ lua/scratch/scratch_file.lua
@@ -205,33 +205,53 @@ function M.fzfScratch()
   -- if telescope loaded, plenary loaded.
   local job = require("plenary.job")
 
+  local function run_and_get_ret(cmd)
+    local ret = true
+    job
+      :new({
+        command = cmd[1],
+        args = vim.list_slice(cmd, 2, #cmd),
+        on_exit = function(_, exit_code)
+          if exit_code ~= 0 then
+            ret = false
+            return
+          end
+        end,
+      })
+      :sync()
+    return ret
+  end
+
+  -- cur: /desktop/
+  -- paths: a/b/main.go
+  -- delete /desktop/a/b/main.go -> /desktop/a/b (if b is empty) -> /desktop/a (if a is empty)
+  local function delete_recursive(i, cur, paths)
+    if i == vim.tbl_count(paths) + 1 then
+      return
+    end
+
+    cur = cur .. utils.Slash() .. paths[i]
+    delete_recursive(i + 1, cur, paths)
+
+    local cmd = {}
+    if vim.fn.isdirectory(cur) == 0 then -- file
+      cmd = { "rm", cur }
+    else -- dir
+      cmd = { "rmdir", cur }
+    end
+
+    local ret = run_and_get_ret(cmd)
+    if ret then
+      vim.notify("Scratch: delete " .. cur .. " successfully", vim.log.levels.INFO)
+    end
+  end
+
   local function delete_item(picker)
     picker:delete_selection(function(s)
-      local path = vim.fn.split(s[1], utils.Slash())
-      -- delete the first dir/file entry
-      local delete_item_name = path[1]
-      local cmd = { "rm", "-rf", scratch_file_dir .. utils.Slash() .. delete_item_name }
-      local ret = true
-      job
-        :new({
-          command = cmd[1],
-          args = vim.list_slice(cmd, 2, #cmd),
-          on_exit = function(_, exit_code)
-            if exit_code ~= 0 then
-              ret = false
-              return
-            end
-          end,
-        })
-        :sync()
+      local paths = vim.fn.split(s[1], utils.Slash())
+      delete_recursive(1, scratch_file_dir, paths)
 
-      if ret then
-        vim.notify("Scratch: delete " .. s[1] .. " successfully", vim.log.levels.INFO)
-      else
-        vim.notify("Scratch: delete " .. s[1] .. " failed", vim.log.levels.ERROR)
-      end
-
-      return ret
+      return true
     end)
   end
 
