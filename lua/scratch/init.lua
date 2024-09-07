7395b8b6bf076ff9db589bcd22231874a8b92888
azan <cornaple@gmail.com>
2024-09-07 19:19:22 +0300

    refactor(config): remove duplicate in `init.lua`

--- lua/scratch/init.lua
+++ lua/scratch/init.lua
@@ -1,28 +1,5 @@
 local M = {}
 
-vim.g.os_sep = vim.g.os_sep or vim.fn.has("win32") and "\\" or "/"
-local base_path = vim.fn.stdpath("cache") .. vim.g.os_sep .. "scratch.nvim" .. vim.g.os_sep
----@type Scratch.ActorConfig
-vim.g.scratch_config = vim.g.scratch_config
-  or {
-    scratch_file_dir = base_path, -- where your scratch files will be put
-    filetypes = { "lua", "js", "py", "sh" }, -- you can simply put filetype here
-    win_config = {
-      relative = "editor", -- Assuming you want the floating window relative to the editor
-      row = 2,
-      col = 5,
-      width = vim.api.nvim_win_get_width(0) - 10, -- Get the screen width - row * col
-      --api_get_option("lines") - 5,
-      height = vim.api.nvim_win_get_height(0) - 5, -- Get the screen height - col
-      style = "minimal",
-      border = "single",
-      title = "",
-    },
-    filetype_details = {},
-    localKeys = {},
-    manual_text = "MANUAL_INPUT",
-  }
-
 ---@class Scratch.FiletypeDetail
 ---@field win_config? vim.api.keyset.win_config
 ---@field content? string[]
@@ -76,8 +53,8 @@ end
 ---@param user_config Scratch.Config
 ---@return Scratch.Actor
 function M.setup(user_config)
-  local junk = { __index = require("scratch.actor") }
-  local config = setmetatable(M.setup_actor(user_config.actor_config), junk)
+  local config =
+    setmetatable(M.setup_actor(user_config.actor_config), { __index = require("scratch.actor") })
   vim.print(config)
   local utils = require("scratch.utils")
   vim.api.nvim_create_user_command("Scratch", function(args)
