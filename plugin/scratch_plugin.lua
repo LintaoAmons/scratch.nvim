bd21b336c7e46f2bf079d214f5580659aa960901
azan <cornaple@gmail.com>
2024-10-21 01:16:50 +0300

    fix(init): get_selected text problem

--- plugin/scratch_plugin.lua
+++ plugin/scratch_plugin.lua
@@ -5,18 +5,21 @@ vim.g.scratch_load = true
 vim.g.os_sep = vim.g.os_sep or vim.uv.os_uname().sysname == "Windows_NT" and "\\" or "/"
 
 ---@return string[]
-local function getSelectedText()
-  return vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."), { type = "v" })
+local function getSelectedText(mark, mode)
+  return vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos(mark), { type = mode })
 end
 local actor = require("scratch.actor")
 vim.g.scratch_config = setmetatable(require("scratch.config"), actor)
 
 vim.api.nvim_create_user_command("Scratch", function(args)
-  if args.range > 0 then
-    vim.g.scratch_config:scratchWithFt({ content = getSelectedText() })
-  else
-    vim.g.scratch_config:scratchWithFt({})
+  local mode = vim.api.nvim_get_mode().mode
+  local opts = {}
+  if mode ~= "n" then
+    opts.content = getSelectedText(".", mode)
+  elseif args.range > 0 then
+    opts.content = getSelectedText("'>", "v")
   end
+  vim.g.scratch_config:scratchWithFt(opts)
 end, { range = true })
 
 vim.api.nvim_create_user_command("ScratchOpen", function()
